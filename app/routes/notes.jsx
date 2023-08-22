ddd1b6ec9671811d558853bfe053eda55f3a4684
swansavage@github.com <swansavage@gmail.com>
2023-08-21 20:16:01 -0500

    added functionality for adding notes

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { redirect } from '@remix-run/node';
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import { getStoredNotes, storeNotes } from '../data/notes';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function NotesPage() {
 	return (
@@ -7,6 +10,26 @@ export default function NotesPage() {
 			<NewNote />
 		</main>
 	);
+}
+
+export async function action({ request }) {
+	const formData = await request.formData();
+
+	// Short Version
+	const noteData = Object.fromEntries(formData);
+
+	// Long Version
+	// const noteData = {
+	// 	title: formData.get('title'),
+	// 	content: formData.get('content')
+	// }
+
+	const existingNotes = await getStoredNotes();
+	noteData.id = uuidv4();
+
+	updatedNotes = existingNotes.concat(noteData);
+	await storeNotes(updatedNotes);
+	return redirect('/notes');
 }
 
 export function links() {

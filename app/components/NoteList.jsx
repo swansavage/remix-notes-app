df3c576daceefd1c455ab0487626dd1ccec629c1
swansavage@github.com <swansavage@gmail.com>
2023-08-21 21:26:02 -0500

    added NotesList component, loader functionality to fetch notes from db on /notes route

---
+++ app/components/NoteList.jsx
@@ -0,0 +1,37 @@
+import styles from './NoteList.css';
+
+//setup Day.js
+const dayjs = require('dayjs');
+var calendar = require('dayjs/plugin/calendar');
+dayjs.extend(calendar);
+
+function NoteList({ notes }) {
+	return (
+		<ul id="note-list">
+			{notes.map((note, index) => (
+				<li key={note.id} className="note">
+					<article>
+						<header>
+							<ul className="note-meta">
+								<li>#{index + 1}</li>
+								<li>
+									<time dateTime={note.date}>
+										Created: {dayjs(note.date).calendar()}
+									</time>
+								</li>
+							</ul>
+							<h2>{note.title}</h2>
+						</header>
+						<p>{note.content}</p>
+					</article>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default NoteList;
+
+export function links() {
+	return [{ rel: 'stylesheet', href: styles }];
+}
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import NoteList, { links as noteListLinks } from '../components/NoteList';
 import { getStoredNotes, storeNotes } from '../data/notes';
 import { v4 as uuidv4 } from 'uuid';
 
+//setup Day.js
+const dayjs = require('dayjs');
+
 export default function NotesPage() {
+	const notes = useLoaderData();
 	return (
 		<main>
 			<NewNote />
+			<NoteList notes={notes} />
 		</main>
 	);
 }
 
+// Remix uses loader to get from db - executed on server, never reaches client
+export async function loader() {
+	const notes = await getStoredNotes();
+	return notes;
+}
+
+// Remix uses form submission to add a note, we use an action here
 export async function action({ request }) {
 	const formData = await request.formData();
 
@@ -26,6 +40,9 @@ export async function action({ request }) {
 
 	const existingNotes = await getStoredNotes();
 	noteData.id = uuidv4();
+	noteData.date = dayjs();
+
+	console.log(noteData.date);
 
 	updatedNotes = existingNotes.concat(noteData);
 	await storeNotes(updatedNotes);
@@ -33,5 +50,5 @@ export async function action({ request }) {
 }
 
 export function links() {
-	return [...newNoteLinks()];
+	return [...newNoteLinks(), ...noteListLinks()];
 }

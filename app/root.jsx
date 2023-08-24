04efa93b1b3fc87a02b05f7d29f4638c7097a3f3
swansavage@github.com <swansavage@gmail.com>
2023-08-24 15:27:24 -0500

    added error handling via ErrorBoundary

--- app/root.jsx
+++ app/root.jsx
@@ -1,12 +1,15 @@
 import { cssBundleHref } from '@remix-run/css-bundle';
 
 import {
+	Link,
 	Links,
 	LiveReload,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	useRouteError,
+	isRouteErrorResponse,
 } from '@remix-run/react';
 
 import styles from '~/styles/main.css';
@@ -44,4 +47,71 @@ export default function App() {
 			</body>
 		</html>
 	);
+}
+
+export function ErrorBoundary() {
+	const error = useRouteError();
+	console.log(error);
+
+	if (isRouteErrorResponse(error)) {
+		return (
+			<html lang="en">
+				<head>
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="width=device-width,initial-scale=1" />
+					<Meta />
+					<Links />
+
+					<title>An error occured!</title>
+				</head>
+				<body>
+					<header>
+						<MainNavigation />
+					</header>
+					<main className="error">
+						<div>
+							<h2>
+								{error.status} {error.statusText}
+							</h2>
+
+							<p>{error.data?.message || 'Something went wrong!'}</p>
+							<p>
+								Back to <Link to="/">safety</Link>!
+							</p>
+						</div>
+					</main>
+				</body>
+			</html>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<html lang="en">
+				<head>
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="width=device-width,initial-scale=1" />
+					<Meta />
+					<Links />
+					<title>An error occured!</title>
+				</head>
+				<body>
+					<header>
+						<MainNavigation />
+					</header>
+
+					<main className="error">
+						<h1>Error</h1>
+						<p>
+							Back to <Link to="/">safety</Link>!
+						</p>
+
+						<p>{error.message}</p>
+						<p>The stack trace is:</p>
+						<p>{error.stack}</p>
+					</main>
+				</body>
+			</html>
+		);
+	} else {
+		return <h1>Unknown Error</h1>;
+	}
 }
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { redirect, json } from '@remix-run/node';
+import {
+	useLoaderData,
+	useRouteError,
+	isRouteErrorResponse,
+	Link,
+} from '@remix-run/react';
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
 import NoteList, { links as noteListLinks } from '../components/NoteList';
 import { getStoredNotes, storeNotes } from '../data/notes';
@@ -24,6 +29,17 @@ export default function NotesPage() {
 // ?this like a GET route?
 export async function loader() {
 	const notes = await getStoredNotes();
+
+	if (!notes || notes.length === 0) {
+		throw json(
+			{ message: 'Could not find any notes.' },
+			{
+				status: 404,
+				statusText: 'Not Found',
+			}
+		);
+	}
+
 	return notes;
 }
 
@@ -59,4 +75,29 @@ export async function action({ request }) {
 
 export function links() {
 	return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function ErrorBoundary() {
+	const error = useRouteError();
+
+	if (isRouteErrorResponse(error)) {
+		return (
+			<>
+				<NewNote />
+				<p className="info-message">{error.data.message}</p>
+			</>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<main className="error">
+				<h1>Oops, something went wrong!</h1>
+				<p>
+					Back to <Link to="/">safety</Link>!
+				</p>
+
+				<p>The stack trace is:</p>
+				<p>{error.stack}</p>
+			</main>
+		);
+	}
 }
